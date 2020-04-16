b4afa21631a0c2536a5acead42cdf23755a4aa92
Doum Kim <eha0129@naver.com>
2020-04-16 17:09:43 +0000

    pagination 수 구하기

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50")
 
-print(indeed_result)
+indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
+
+pagination = indeed_soup.find("div",{"class":"pagination"})
+
+pages = pagination.find_all("a")
+
+spans = []
+
+for page in pages:
+  spans.append(page.find("span"))
+
+print(spans[:-1])
